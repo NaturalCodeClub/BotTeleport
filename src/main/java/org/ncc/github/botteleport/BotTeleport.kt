8150d186d65976c9d7c28e7703dcd0a57bef9957
NatJerry <xiaomajunqwq123@outlook.com>
2024-02-07 21:22:25 +0800

    基本完成 增加/bottp reload指令

--- src/main/java/org/ncc/github/botteleport/BotTeleport.kt
+++ src/main/java/org/ncc/github/botteleport/BotTeleport.kt
@@ -1,30 +1,91 @@
 package org.ncc.github.botteleport
 
-import org.bukkit.Bukkit
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.plugin.java.JavaPlugin
+import org.jetbrains.annotations.NotNull
 import org.ncc.github.botteleport.command.commandBotTp
+import java.util.logging.Level
 import java.util.logging.Logger
 
 class BotTeleport : JavaPlugin() {
-    @JvmField val l:Logger = logger
-    @JvmField
-    var plugin: JavaPlugin? = null
+
+
+    var config: FileConfiguration? = null
+    var configVer = 1
+
+    companion object {
+        lateinit var l: Logger
+        lateinit var mistakeStr: String
+        lateinit var successStr: String
+        lateinit var nopermissionStr: String
+        lateinit var notPlayerStr: String
+        lateinit var reloadMsgStr: String
+        lateinit var notExistStr: String
+
+        var plugin: JavaPlugin? = null
+        fun configReload(s: String): String {
+            plugin?.reloadConfig()
+            loadConfig(plugin!!.config, plugin!!)
+            return s
+        }
+
+        private fun loadConfig(
+            @NotNull f: FileConfiguration,
+            @NotNull p: JavaPlugin
+        ) {
+            if (f.getString("mistake") != null) mistakeStr = f.getString("mistake")!!
+            if (f.getString("success") != null) successStr = f.getString("success")!!
+            if (f.getString("notplayer") != null) notPlayerStr = f.getString("notplayer")!!
+            if (f.getString("notexist") != null) notExistStr = f.getString("notexist")!!
+            if (f.getString("nopermission") != null) nopermissionStr = f.getString("nopermission")!!
+            if (f.getString("reloadMsg") != null) reloadMsgStr = f.getString("reloadMsg")!!
+        }
+    }
+
     override fun onEnable() {
         l.info("BotTeleport plugin enabling")
-        val c = commandBotTp()
-        getCommand("bottp")?.setExecutor(c)
-        getCommand("bottp")?.tabCompleter = c
         plugin = getPlugin(this)
+        config = getConfig()
+        l = plugin!!.logger
+        checkConfig(config!!, plugin!!)
+        loadConfig(config!!, plugin!!)
+        getCommand("bottp")?.setExecutor(commandBotTp())
+        getCommand("bottp")?.tabCompleter = commandBotTp()
+
     }
 
     override fun onDisable() {
         l.info("BotTeleport plugin disabling")
+        saveConfig()
         getCommand("bottp")?.setExecutor(null)
     }
+
     private fun getPlugin(plugin: JavaPlugin): JavaPlugin {
         return plugin
     }
 
+
+    private fun checkConfig(@NotNull f: FileConfiguration, @NotNull p: JavaPlugin) {
+        if (f.get("config-version") != null) {
+            if (f.getInt("config-version") != configVer) {
+                p.logger.log(Level.SEVERE, "BotTeleport配置文件出错，请检查，或删除其重新生成")
+                p.logger.log(
+                    Level.SEVERE,
+                    "The config of BotTeleport went wrong,please check,or just delete it to regenerate new config file"
+                )
+                p.onDisable()
+            }
+        } else {
+            f.set("config-version", configVer)
+        }
+        if (f.get("mistake") == null) f.set("mistake", "指令使用方法有误")
+        if (f.get("success") == null) f.set("success", "假人传送成功")
+        if (f.get("notplayer") == null) f.set("notplayer", "您不是一个玩家")
+        if (f.get("notexist") == null) f.set("notexist", "机器人不存在")
+        if (f.get("nopermission") == null) f.set("notpermission", "你没有权限")
+        if (f.get("reloadMsg") == null) f.set("reloadMsg", "重载配置文件成功")
+        p.saveConfig()
+    }
 
 
 }
--- src/main/java/org/ncc/github/botteleport/command/commandBotTp.kt
+++ src/main/java/org/ncc/github/botteleport/command/commandBotTp.kt
@@ -5,6 +5,14 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabExecutor
 import org.bukkit.entity.Player
+import org.ncc.github.botteleport.BotTeleport
+import org.ncc.github.botteleport.BotTeleport.Companion.configReload
+import org.ncc.github.botteleport.BotTeleport.Companion.mistakeStr
+import org.ncc.github.botteleport.BotTeleport.Companion.nopermissionStr
+import org.ncc.github.botteleport.BotTeleport.Companion.notExistStr
+import org.ncc.github.botteleport.BotTeleport.Companion.notPlayerStr
+import org.ncc.github.botteleport.BotTeleport.Companion.reloadMsgStr
+import org.ncc.github.botteleport.BotTeleport.Companion.successStr
 import top.leavesmc.leaves.entity.Bot
 
 class commandBotTp : TabExecutor {
@@ -22,25 +30,32 @@ class commandBotTp : TabExecutor {
     }
 
     override fun onCommand(sender: CommandSender, command: Command, p2: String, args: Array<out String>?): Boolean {
+        if (!sender.hasPermission("bukkit.command.bot")) {
+            sender.sendMessage(nopermissionStr)
+            return true
+        }
         if (sender !is Player) {
-            sender.sendMessage("您不是一个玩家")
+            sender.sendMessage(notPlayerStr)
             return false
         }
         if (args!!.size != 2) {
-            sender.sendMessage("指令使用方法有误")
+            sender.sendMessage(mistakeStr)
             return false
         }
         //上述判断阻止非法的指令使用方法导致插件出现问题
         val s: String = args[1]
+        if (s.equals("reload", true)) {
+            sender.sendMessage(configReload(reloadMsgStr))
+        }
         val b: Bot? = Bukkit.getBotManager().getBot(s)
         if (b == null) {
-            sender.sendMessage("机器人不存在")
+            sender.sendMessage(notExistStr)
             return false
         }
 
         b.teleport(sender)
 
-        sender.sendMessage("传送成功")
+        sender.sendMessage(successStr)
         return true
     }
 
