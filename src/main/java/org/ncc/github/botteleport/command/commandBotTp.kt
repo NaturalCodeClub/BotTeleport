63f9d64751b3e54892e9db16a7ce249bc6a8a8fb
NatJerry <xiaomajunqwq123@outlook.com>
2024-02-08 20:13:21 +0800

    修复了我的唇笔bug

--- src/main/java/org/ncc/github/botteleport/command/commandBotTp.kt
+++ src/main/java/org/ncc/github/botteleport/command/commandBotTp.kt
@@ -11,6 +11,7 @@ import org.ncc.github.botteleport.BotTeleport.Companion.mistakeStr
 import org.ncc.github.botteleport.BotTeleport.Companion.nopermissionStr
 import org.ncc.github.botteleport.BotTeleport.Companion.notExistStr
 import org.ncc.github.botteleport.BotTeleport.Companion.notPlayerStr
+import org.ncc.github.botteleport.BotTeleport.Companion.notSameWorldStr
 import org.ncc.github.botteleport.BotTeleport.Companion.reloadMsgStr
 import org.ncc.github.botteleport.BotTeleport.Companion.successStr
 import top.leavesmc.leaves.entity.Bot
@@ -31,31 +32,35 @@ class commandBotTp : TabExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, p2: String, args: Array<out String>?): Boolean {
         if (!sender.hasPermission("bukkit.command.bot")) {
-            sender.sendMessage(nopermissionStr)
+            nopermissionStr?.let { sender.sendMessage(it) }
             return true
         }
         if (sender !is Player) {
-            sender.sendMessage(notPlayerStr)
+            notPlayerStr?.let { sender.sendMessage(it) }
             return false
         }
-        if (args!!.size != 2) {
-            sender.sendMessage(mistakeStr)
+        if (args!!.size != 1) {
+            mistakeStr?.let { sender.sendMessage(it) }
             return false
         }
         //上述判断阻止非法的指令使用方法导致插件出现问题
-        val s: String = args[1]
+        val s: String = args[0]
         if (s.equals("reload", true)) {
-            sender.sendMessage(configReload(reloadMsgStr))
+            val s = reloadMsgStr?.let { configReload(it) }
+            sender.sendMessage(s!!)
         }
         val b: Bot? = Bukkit.getBotManager().getBot(s)
         if (b == null) {
-            sender.sendMessage(notExistStr)
+            notExistStr?.let { sender.sendMessage(it) }
             return false
+        }
+        if (sender.world != b.world) {
+            notSameWorldStr?.let { sender.sendMessage(it) }
         }
 
         b.teleport(sender)
 
-        sender.sendMessage(successStr)
+        successStr?.let { sender.sendMessage(it) }
         return true
     }
 
